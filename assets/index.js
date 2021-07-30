6fd44f67103025fc4ddc1e39cf033a77d397e62b
Ethan Nguyen <ethanvanng@gmail.com>
2021-07-29 20:57:32 -0700

    Fixes to searchbar

--- assets/index.js
+++ assets/index.js
@@ -6,3 +6,79 @@
     Do not put any api requests here.
 
 */
+
+var clearIcon = $(".clear-icon");
+var searchBar = $(".search");
+var searchInput= "";
+
+
+// searchBar.on("keyup", () => {
+ 
+//   if(searchBar.val() && clearIcon.css("display:none")){
+//     clearIcon.css("display:block")
+//   } else if(!searchBar.val()) {
+//     clearIcon.css("visibility:none");
+//   }
+// });
+
+
+//searchBar.change(function(){
+searchBar.on("keyup", () => {
+  console.log('change');
+  if(searchBar.val() == ''){
+    clearIcon.addClass("hidden")
+  }else{
+    clearIcon.removeClass("hidden")
+  }
+});
+
+searchBar.keypress(function(event){
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '13'){
+
+        searchInput=searchBar.val().trim();
+        console.log(searchInput);
+        search(searchInput);
+    }
+});
+
+clearIcon.on("click", function(){
+    searchBar.val("");
+    clearIcon.addClass("hidden");
+})
+
+
+
+function search(searchTerm){
+  var mainEl = $('main').empty();
+  mainEl.text(searchTerm);
+}
+
+
+//search funtion paramenter of keyword
+
+
+
+/*  non Jquery version of searchbar code
+ $(".clear-icon").on("click",function(){
+  searchBar.val()=="";
+  clearIcon.css("visibility:hidden");
+});
+
+var clearIcon = document.querySelector(".clear-icon");
+var searchBar = document.querySelector(".search");
+
+searchBar.addEventListener("keyup", () => {
+ console.log(searchBar.value);
+  if(searchBar.value && clearIcon.style.visibility != "visible"){
+    clearIcon.style.visibility = "visible";
+  } else if(!searchBar.value) {
+    clearIcon.style.visibility = "hidden";
+  }
+});
+
+clearIcon.addEventListener("click", () => {
+  searchBar.value = "";
+  clearIcon.style.visibility = "hidden";
+})
+*/
