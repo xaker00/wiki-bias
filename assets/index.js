db70aca7a9fe070bd1f5a2891e1fa1535d8672b7
Ilya Spivakov <ilya@spivakov.me>
2021-07-29 18:29:22 -0700

    Changes by Ilya and Steph

--- assets/index.js
+++ assets/index.js
@@ -6,3 +6,35 @@
     Do not put any api requests here.
 
 */
+
+processText("This is a very boring project", processSentiment);
+processText("This is a  fun project", processSentiment);
+
+function processSentiment(data){
+    console.log(data);
+}
+
+
+wikiLatestEdits(function(data){
+    console.log('data from callback', data);
+});
+
+
+wikiSearch('donald trump', function(data){
+    console.log('data from callback', data);
+
+    pageIds=data[4];
+
+    console.log(pageIds);
+
+    wikiText(pageIds[0], function(data2){
+        console.log(data2);
+        processText(data2, processSentiment);
+
+});
+});
+
+// wikiText(4848272, function(data){
+//     console.log(data);
+//     processText(data, processSentiment);
+// });
