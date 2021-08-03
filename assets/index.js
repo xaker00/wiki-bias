8d0af35543d51b040009eebd363b97344a12455e
Ilya Spivakov <ilya@spivakov.me>
2021-08-02 21:36:03 -0700

    Working API & whitespace updates

--- assets/index.js
+++ assets/index.js
@@ -7,92 +7,87 @@
 
 */
 
-var clearIcon = $(".clear-icon");
-var searchBar = $(".search");
-var searchInput= "";
+const clearIcon = $(".clear-icon");
+const searchBar = $(".search");
+var searchInput = "";
 
-
-// searchBar.on("keyup", () => {
- 
-//   if(searchBar.val() && clearIcon.css("display:none")){
-//     clearIcon.css("display:block")
-//   } else if(!searchBar.val()) {
-//     clearIcon.css("visibility:none");
-//   }
-// });
-
-
-//searchBar.change(function(){
 searchBar.on("keyup", () => {
-  console.log('change');
-  if(searchBar.val() == ''){
-    clearIcon.addClass("hidden")
-  }else{
-    clearIcon.removeClass("hidden")
+  console.log("change");
+  if (searchBar.val() == "") {
+    clearIcon.addClass("hidden");
+  } else {
+    clearIcon.removeClass("hidden");
   }
 });
 
-searchBar.keypress(function(event){
-    var keycode = (event.keyCode ? event.keyCode : event.which);
-    if(keycode == '13'){
-        searchInput=searchBar.val().trim();
-        console.log(searchInput);
-        search(searchInput);
-    }
+searchBar.keypress(function (event) {
+  const keycode = event.keyCode ? event.keyCode : event.which;
+  if (keycode == "13") {
+    searchInput = searchBar.val().trim();
+    console.log(searchInput);
+    search(searchInput);
+  }
 });
 
-clearIcon.on("click", function(){
-    searchBar.val("");
-    clearIcon.addClass("hidden");
-})
+clearIcon.on("click", function () {
+  searchBar.val("");
+  clearIcon.addClass("hidden");
+});
 
-
-function renderSearchBar(){
-  var searchBarResultPage = $('<div>').attr();
-  $('main').prepend(searchBarResultPage);
-  
+function renderSearchBar() {
+  var searchBarResultPage = $("<div>").attr();
+  $("main").prepend(searchBarResultPage);
 }
 
 function search(searchTerm) {
-    var mainEl = $('main').empty();
+  var mainEl = $("main").empty();
 
-    renderSearchBar();
+  //renderSearchBar();
 
-    mainEl.text(searchTerm);
-    wikiSearch(searchTerm)
-        .then(function (data) {
-            console.log('data from callback', data);
+  mainEl.text(searchTerm);
+  wikiSearch(searchTerm).then(function (data) {
+    //console.log('data from callback', data);
 
-            pageIds = data[4];
+    pageIds = data[4];
 
-            //render search results here    
-            displaySearchResults(data); 
-            
+    //render search results here
+    displaySearchResults(data);
 
-            //console.log('page IDs', pageIds);
+    //console.log('page IDs', pageIds);
 
-            Promise.all(
-                pageIds.map(id => wikiText(id))
-                //textArray
-            )
-            .then((text) => console.log(text));
+    Promise.all(
+      pageIds.map((id) => wikiText(id))
+      //textArray
+    ).then((text) => {
+      //console.log(text);
+      for (let i = 0; i < text.length; i++) {
+        displayDescription(i, shorten(text[i], 200) + "...");
+
+        processText(text[i]).then((scoreObj) => {
+          displaySentimentScore(i, scoreObj.score);
         });
+      }
+    });
+  });
 
-        console.log("fdsfdasf");
+  console.log("fdsfdasf");
 }
 
-function displayDescription(i, data){
-  const descriptionEl = $('#description' + i).empty();
+// https://stackoverflow.com/a/40382963
+// Shorten a string to less than maxLen characters without truncating words.
+function shorten(str, maxLen, separator = " ") {
+  if (str.length <= maxLen) return str;
+  return str.substr(0, str.lastIndexOf(separator, maxLen));
+}
+
+function displayDescription(i, data) {
+  const descriptionEl = $("#description" + i).empty();
   descriptionEl.text(data);
-  
-  //var rowE3 = $('<div>').attr('id', 'description').text(" DESCRIPTION: "+data2.substring(0,200)+("...")); //adds a div containing the first 200 letters of the wiki page.
-  // $('main').append(rowE3);
 }
 
+function displaySearchResults(data) {
+  console.log("displaySearchResults data", data);
 
-function displaySearchResults(data){
-  console.log('displaySearchResults data', data);
-  
   /* data array 
   0 search term
   1 titles
@@ -101,27 +96,22 @@ function displaySearchResults(data){
   4 page id
   */
 
-  var titles = data[1];
-  var urls = data[3];
-  var pageID=data[4];
-  
-  for(var i =0; i<titles.length; i++){
+  const titles = data[1];
+  const urls = data[3];
+  //var pageID = data[4];
 
-    //var rowEl = $('<div>').attr('id', 'result'+ i).text(titles[i]);
-    //var rowE2 = $('<div>').attr('id', 'result'+ i).text(urls[i]);
-
-    const containerEl = $('<div>');
-    const titleEl = $('<div>').text(titles[i]);
-    const urlEl = $('<a>').text(urls[i]).attr("href", urls[i]);
-    const descriptionEl = $('<div>').attr('id', 'description'+ i);
-    const sentimentEl = $('<div>').attr('id', 'sentiment'+ i);
+  for (var i = 0; i < titles.length; i++) {
+    const containerEl = $("<div>");
+    const titleEl = $("<div>").text(titles[i]);
+    const urlEl = $("<a>").text(urls[i]).attr("href", urls[i]);
+    const descriptionEl = $("<div>").attr("id", "description" + i);
+    const sentimentEl = $("<div>").attr("id", "sentiment" + i);
 
     containerEl.append(titleEl, urlEl, descriptionEl, sentimentEl);
 
-    $('main').append(containerEl);
-    
+    $("main").append(containerEl);
 
-/*
+    /*
 <div id=result1>
     <div> title
     <div> URL
@@ -130,89 +120,46 @@ function displaySearchResults(data){
 </div>
 
 */
-
-
-
   }
 
-  //$('main').append(rowEl);
-  //$('main').append(rowE2);
-  
-
-  // for testing only - delete once API is working correctly
-  for(let x=0; x<10; x++){
-    displayDescription(x, 'Loading... ' + (1+x));
-    displaySentimentScore(x, 'score ' + (1+x));
+  // Show placeholder text
+  for (let x = 0; x < 10; x++) {
+    displayDescription(x, "Loading Description... ");
+    displaySentimentScore(x, "Loading Score... ");
   }
-
-   
-  
 }
 
-function displaySentimentScore(i, score){
-  const sentimentEl = $('#sentiment' + i).empty();
+function displaySentimentScore(i, score) {
+  const sentimentEl = $("#sentiment" + i).empty();
   sentimentEl.text(score);
 }
 
-//search funtion paramenter of keyword
-
-
-
-/*  non Jquery version of searchbar code
- $(".clear-icon").on("click",function(){
-  searchBar.val()=="";
-  clearIcon.css("visibility:hidden");
-});
-
-var clearIcon = document.querySelector(".clear-icon");
-var searchBar = document.querySelector(".search");
-
-searchBar.addEventListener("keyup", () => {
- console.log(searchBar.value);
-  if(searchBar.value && clearIcon.style.visibility != "visible"){
-    clearIcon.style.visibility = "visible";
-  } else if(!searchBar.value) {
-    clearIcon.style.visibility = "hidden";
-  }
-});
-
-clearIcon.addEventListener("click", () => {
-  searchBar.value = "";
-  clearIcon.style.visibility = "hidden";
-})
-*/
+/*
 function apiTests() {
-    processText("This is a very boring project").then(processSentiment);
-    processText("This is a  fun project").then(processSentiment);
+  processText("This is a very boring project").then(processSentiment);
+  processText("This is a  fun project").then(processSentiment);
 
-    function processSentiment(data) {
-        console.log(data);
-    }
+  function processSentiment(data) {
+    console.log(data);
+  }
 
+  wikiLatestEdits().then(function (data) {
+    console.log("data from callback", data);
+  });
 
-    wikiLatestEdits()
-        .then(function (data) {
-            console.log('data from callback', data);
-        });
+  wikiSearch("Walt disney").then(function (data) {
+    console.log("data from callback", data);
 
+    pageIds = data[4];
 
-    wikiSearch('Walt disney')
-        .then(function (data) {
-            console.log('data from callback', data);
+    console.log("page IDs", pageIds);
 
-            pageIds = data[4];
-
-            console.log('page IDs', pageIds);
-
-            wikiText(pageIds[0])
-                .then(function (data2) {
-                    console.log(data2);
-                    processText(data2).then(processSentiment);
-
-                });
-        });
-
+    wikiText(pageIds[0]).then(function (data2) {
+      console.log(data2);
+      processText(data2).then(processSentiment);
+    });
+  });
 }
 
-apiTests();
- 
+//apiTests();
+*/
